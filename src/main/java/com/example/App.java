2d48c90ee171ccb1b1398872f3118a5ff2c23045
Soumya-2021wb86933 <2021wb86933@wilp.bits-pilani.ac.in>
2025-07-14 18:53:19 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins CI!",CI Build is Successful);
+        System.out.println("Hello, Jenkins CI!",CI Build is Successfully completed);
     }
 }
